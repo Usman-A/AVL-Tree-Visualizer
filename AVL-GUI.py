ed31edbe2a40c6772e142b30e8eac2283af98dad
Usman Asad <asadu@mcmaster.ca>
2020-10-16 18:38:50 -0400

    Started working on drawing nodes, current build not working, switching PC'S atm

--- AVL-GUI.py
+++ AVL-GUI.py
@@ -18,6 +18,13 @@ class Main:
         #Boolean stating that tree is empty
         self.empty = True
 
+        #Storing initial coodrinate for first node
+        self.x0 = self.width/2 - 20
+        self.y0 = 10
+        self.x1 = self.width/2 + 20
+        self.y1 = 50
+        self.delta = 20
+
         # Creating a window and adding labels
         frame1 = Frame(window)
         frame1.pack()
@@ -36,12 +43,39 @@ class Main:
     #draws a circle with a number in it
     def createNode(self,x,y,data):
         r = 20
-        self.canvas.create_oval(x-r,y-r,x+r,y+r, tags = "tree")
+        self.canvas.create_oval(x-r,y-r,x+r,y+r, tags = "tree")        
         self.canvas.create_text(x-6, y, anchor=W, text=data, tags = "tree")
-        print(self.avl_tree)
+        # print(self.avl_tree)
 
     def error(self):
         self.canvas.create_text(self.width/2 - 100, self.height/2, anchor=W, text="Please make sure your input is a number", tags = "tree")
+
+    #Testing preorderTraversal code
+    #Passing a tree, a boolean pos, left is false right is true
+    def preOrderTrav(self,tree, pos):
+
+        if (tree == None):
+            return
+
+        #Number of levels between initial and last, will help in calcuating the positions of he nodes
+        depth = tree.getDepth()
+
+        if (depth == 0):
+            self.createNode(300,30,tree.data)
+            return
+
+        #X will be positive if going right, negative if going left
+        #y will remain negative
+        # scale movement by depth
+        
+        #create left
+        if (not pos):
+            self.createNode(x, tree.data)ls
+        else:
+            self.createNode(x,y,tree.data)
+        self.preOrderTrav(tree.left, False)
+        self.preOrderTrav(tree.right,True)
+
 
 
     def display(self):
@@ -59,9 +93,12 @@ class Main:
                 self.empty = False
             # Otherwise insert in tree             
             else:
-                self.avl_tree = self.avl_tree.insert(userInput)     
-            
+                self.avl_tree = self.avl_tree.insert(userInput)  
+
             # drawNode -- This should later be changed to call a drawTree function
+            if (userInput == 12):
+                self.preOrderTrav(self.avl_tree)   
+            
             return self.createNode(300,30, userInput)
         except:
             return self.error()       
